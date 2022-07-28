a2ef57a6a4d42be952bf876eeee154b4e1febf7a
Sanjeev Shrestha <name>
2022-07-28 10:13:22 +0545

    Classwork 2 meter to feet

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -7,8 +7,24 @@
 // Console.WriteLine($"Hello, {firstName}!");
 
 //Class work 1
+// Program to capitalize entered text.
 
-Console.Write("Enter your Name:");
-var name = Console.ReadLine();
-var uppercase = name.ToUpper();
-Console.WriteLine(uppercase);
+        // Console.Write("Enter meter:");
+        // int meter = Console.ReadLine();
+        // var feet = meter.();
+        // Console.WriteLine(uppercase);
+
+//Class Work 2
+//Program to convert meter to feet. Output: n metre = m feet
+
+/* double meter, feet;
+Console.WriteLine("please enter amount of metres");
+feet = Convert.ToInt32(Console.ReadLine());
+meter = feet * 3.2808399;
+Console.WriteLine("\nFeet in meter : " + meter); */
+
+Console.Write("Enter Meter:");
+var length = Console.ReadLine();
+var lengthNumber = double.Parse(length);
+var feets = lengthNumber * 3.2808399;
+Console.WriteLine($"{lengthNumber}metres = {feets} feet");
