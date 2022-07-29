9f422caa44c1d6aaeacb3af3fc0082de77c98a0f
Sanjeev Shrestha <name>
2022-07-29 21:45:20 +0545

    Program to convert days to years, months, weeks and days.

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -1,6 +1,37 @@
-﻿using System;
+﻿/* using System;
+Console.Write("Enter number of days: ");
+double numberofdays = int.Parse(Console.ReadLine());
+double years = numberofdays/365;
+double months = numberofdays/30;
+double weeks = numberofdays/7;
+Console.WriteLine($"The {numberofdays} days is equivalent to {years} Years, {months} Months, {weeks} Weeks and {numberofdays} Days."); */
+/* using System;
+        int ndays, year, month, week, days, DAYSINWEEK=7;
+        Console.WriteLine("Enter the number of days");
+        ndays = int.Parse(Console.ReadLine());
+        year = ndays / 365;
+        month = (ndays % 365) / DAYSINWEEK;
+        week = (ndays % 365) / DAYSINWEEK;
+        days = (ndays % 365) % DAYSINWEEK;
+        Console.WriteLine("{0} is equivalent to {1}years, {2}weeks and {3}days", ndays, year, week, days); */
 
-Console.Write("Enter radius: ");
-var radius = Convert.ToDouble(Console.ReadLine());
-var Area = Math.PI * radius * radius;
-Console.WriteLine("Area of circle: " + Area);
+    using System;
+        double num     =0;
+        double years   =0;
+        double months =0;
+        double weeks   =0;
+        double days    =0;
+        
+        
+        Console.Write("Enter number of days: ");
+        num = Convert.ToInt32(Console.ReadLine());
+        
+        years = num / 365;
+        months = num / 30.41;
+        weeks = (num % 365) / 7;
+        days  = (num % 365) % 7;
+
+        Console.WriteLine("Years : " + years);
+        Console.WriteLine("Months :" + months);
+        Console.WriteLine("Weeks : " + weeks);
+        Console.WriteLine("Days  : " + days );
