7963449f7d298a0d7a67fe3902121791d545881f
Sanjeev Shrestha <name>
2022-08-04 11:02:16 +0545

    Constructor

--- CSharpFundamentals/Lessons/0-EntryPoint.cs
+++ CSharpFundamentals/Lessons/0-EntryPoint.cs
@@ -4,7 +4,7 @@ class TestClasses
 {
     public static void Main()
     {
-/*         Animal cow = new();
+/*      Animal cow = new();
         Animal goat = new();
         Animal buffalo = new();
         Animal deer = new();
@@ -15,12 +15,15 @@ class TestClasses
         cow.PrintDetails(); */
         ;/* BranchesAndLoops br = new();
         br.LearLooping(); */
-        MethodLearning ml = new();
+        /* MethodLearning ml = new(); */
         /* ml.PrintNepal();
         ml.PrintNepal(10);
         var x = ml.GetInitials("Saneev Kumar Shrestha"); */
-        short[] numbers = {12, 15, 2, -5};
+        /* short[] numbers = {12, 15, 2, -5};
         var (min, max, Average) = ml.GetMinMaxAverage(numbers);
-        Console.WriteLine($"Minimum: {min}, Maxmimum: {max}, Average Value : {Average}");
+        Console.WriteLine($"Minimum: {min}, Maxmimum: {max}, Average Value : {Average}"); */
+
+        Shape2D shape = new("Rectangle");
+        shape.PrintDetail();
     }
 }
